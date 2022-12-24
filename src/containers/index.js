588c241ab2913d7214f5839d15b512aa9bf5421a
hamidreza <avesta_shivana@yahoo.com>
2022-12-24 19:41:37 +0330

    secend commit

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { defult as Blog } from "./blog/Blog";
+export { defult as Features } from "./features/Features";
+export { defult as Footer } from "./footer/Footer";
+export { defult as Header } from "./header/Header";
+export { defult as Possibility } from "./possibility/Possibility";
+export { defult as WhatGPT3 } from "./whatGPT3/WhatGPT3";
